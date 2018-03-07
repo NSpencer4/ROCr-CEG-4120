2b852565b1464fb92bdf40a661c58310f6f7b33f
JerGorJac <jacobson.9@wright.edu>
2018-03-07 18:41:31 -0500

    had to put urls for tasks in rocr/urls

--- rocr/rocr/urls.py
+++ rocr/rocr/urls.py
@@ -3,11 +3,15 @@ from django.contrib import admin
 from django.conf.urls.static import static
 #from django.conf.urls.media import media
 from django.conf import settings
+import Media.views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('Media.urls', namespace="Media")),
-    url('^', include('django.contrib.auth.urls'))
+    url('^', include('django.contrib.auth.urls')),
+    url(r'^task1$', Media.views.task1, name='task1'),
+    url(r'^task2$', Media.views.task2, name='task2'),
+    url(r'^task3$', Media.views.task3, name='task3'),
 ]
 
 if settings.DEBUG:
